2b8d221111c3d2a6e75babff6816f90ad75844be
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2022-07-17 17:18:11 +0530

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,27 @@
+words = ("bag","car","van","pan")
+my_list = list(words)
+my_list.remove("bag")
+print(my_list)
+
+Output
+['car', 'van', 'pan']
+
+Question2
+a = {
+    1:"a",
+    2:"b"
+    }
+a.update({3:"4"})
+print(a)
+
+Output
+{1: 'a', 2: 'b', 3: '4'}
+
+Question3
+colour = {"red","green","white","orange","blue"}
+print(len(colour))
+
+Output
+
+5
+
