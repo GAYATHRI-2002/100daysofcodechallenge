bea3b7ba94d06272dd1d27fa44bec6825d32a103
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2023-03-07 19:29:31 +0530

    Update day7.py

--- DAYS/day7.py
+++ DAYS/day7.py
@@ -4,3 +4,17 @@ area = length * breadth
 perimeter = 2 * (length + breadth)
 print("the area is: ",area)
 print("the perimeter is: ", perimeter)
+
+
+sub_1 = int(input("enter the first subject mark: "))
+sub_2 = int(input("enter the second subject mark: "))
+sub_3 = int(input("enter the third subject mark: "))
+sub_4 = int(input("enter the fourth subject mark: "))
+
+total = sub_1 + sub_2 + sub_3 + sub_4
+average = total / 4
+
+if total > 50:
+    print("pass")
+else:
+    print("fail") 
