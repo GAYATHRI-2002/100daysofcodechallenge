9ef997887b0cfbeeafc96b754bdb50e0b32f6767
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2023-03-07 19:21:36 +0530

    Create day7.py

---
+++ DAYS/day7.py
@@ -0,0 +1,6 @@
+length = int(input("enter the length: "))
+breadth = int(input("enter the breadth: "))
+area = length * breadth
+perimeter = 2 * (length + breadth)
+print("the area is: ",area)
+print("the perimeter is: ", perimeter)
