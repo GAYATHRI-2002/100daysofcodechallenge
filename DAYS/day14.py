37a8dc54b45c6184a911f0d27e92cb45ec42fcb7
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2023-05-12 22:31:42 +0530

    Create day14.py

---
+++ DAYS/day14.py
@@ -0,0 +1,23 @@
+counter = 0
+while counter < 6:
+  print(counter)
+  counter += 1
+
+  -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
+  
+  print("THE ANIMAL LIST ARE ...cow....A lesser......")
+
+exit = "no"
+while exit == "no":
+  animal  = input("What animal do you want?: ")
+  if animal == "cow":
+    print("A cow goes moooo....")
+  elif animal == "A lesser":
+    print("""Spotted lemur
+    ummm.... the lesser spotted lemur goes awooga.""")
+  else:
+    print("i don't know what are you talking....")
+    
+  exit = input("Do you want to exit?: ")
+  
