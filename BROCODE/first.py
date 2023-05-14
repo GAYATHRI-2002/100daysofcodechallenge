0818370768f61150fb342c422cf6db614431297f
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2023-05-14 12:35:32 +0530

    Create first.py

---
+++ BROCODE/first.py
@@ -0,0 +1,47 @@
+print("i love pizza")
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+first_name = "Sam"
+last_name = "smith"
+full_name = first_name +" " + last_name
+
+print("hello " +full_name)
+print(type(name))
+-----------------------------------------------------------------------------------------------------------------------------------------------------
+age = 21
+age = age +5
+age += 5
+print("your age is "+str(age))
+print(type(age))
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+heigth = 156.5
+print("your height is :  "+str(heigth)+"cm")
+print(type(heigth))
+--------------------------------------------------------
+name = "sam"
+age = 21
+attractive = True
+name, age, attractive = "sam", 21, True
+
+print(name)
+print(age)
+print(attractive)
+
+ravi = sam = meena = 30
+print(ravi)
+print(sam)
+print(meena)
+-------------------------------------
+name = "sam"
+
+print(len(name))
+print(name.find("a"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("a"))
+print(name.replace("a", "i"))
+print(name*3)
+
+
