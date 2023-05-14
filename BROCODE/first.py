9c9953a53786893db25e7193f3750be0939558cc
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2023-05-14 17:36:24 +0530

    Update first.py

--- BROCODE/first.py
+++ BROCODE/first.py
@@ -43,5 +43,54 @@ print(name.isalpha())
 print(name.count("a"))
 print(name.replace("a", "i"))
 print(name*3)
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+x = 5
+y = 3.0
+z = "1"
+
+x = int(x)
+y = int(y)
+z = int(z)
+
+
+print("x is a  "+str(x))
+print(y)
+print(z*5)
+-----------------------------------------------------------------------------------------------------------------------
+name = input("What is your name?: ")
+age = int(input("how old are you?: "))
+height = float(input("how tall are you?: "))
+
+
+print("hello "+name)
+print("you are "+str(age)+" years old")
+print("you are "+str(height)+" cm tall")
+----------------------------------------------------------------------------------------------------------------------------
+pi = 3.14
+x = 1
+y = 2
+z = 3
+
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(pi))
+print(pow(pi, 2))
+print(math.sqrt(20))
+print(max(x, y, z))
+print(min(x, y, z))
+---------------------------------------------------------------------------------------------------
+name = "sam smith"
+first_name = name[0:3]
+last_name = name[4:9]
+funky__name = name[0:9:2]
+reversed_name = name[::-1]
+print(first_name)
+print(last_name)
+print(funky__name)
+print(reversed_name)
+-------------------------------------------------------------------------------
+
 
 
