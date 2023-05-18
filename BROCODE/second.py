a8c5d552a3051d83907b595dc34bb1fdc464d997
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2023-05-18 11:03:43 +0530

    Update second.py

--- BROCODE/second.py
+++ BROCODE/second.py
@@ -14,3 +14,48 @@ elif age < 0:
     print("you haven't born yet")
 else:
     print("you are a child")
+    -------------------------------------------------------
+    
+    # logical operators 
+
+temp = int(input("what is the temperature outside: "))
+
+if not(temp >=0 and temp <= 30) :
+    print (" the temperature good today")
+    print("go out side")
+
+elif not(temp < 0 or temp >30) :
+    print("the temperature is bad today")
+    
+ --------------------------------------------------------------
+name = None
+
+while not name:
+    name = input("enter the name: ")
+print("hello "+name)
+----------------------------------------------------------
+#for loop
+
+#for i in range(10):
+    #print(i+1)
+
+for i in range (50, 100+1, 2):
+    print(i)
+  ---------------------------
+for seconds in range(20,0,-2):
+    print(seconds)
+    time.sleep(1)
+print("HAPPY NEW YEAR")
+-----------------------------------------------
+#NESTED LOOP
+
+row = int(input("how many rows: "))
+column = int(input("how many columns: "))
+symbol = input("what symbol should be printed: ")
+
+for i in range (row):
+    for j in range(column):
+        print(symbol, end="")
+    print()
+
+
