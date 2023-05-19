df8711f3f1c2b217aeae937011208d7501cb2e7b
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2023-05-19 19:54:26 +0530

    Update third.py

--- BROCODE/third.py
+++ BROCODE/third.py
@@ -2,3 +2,30 @@ while True:
     name = input("enter your name: ")
     if name != "":
         break
+  ------------------------
+#phone_number = "123-456-789"
+#for i in phone_number:
+ #   if i == "-":
+  #      continue
+   # print(i,end="")
+
+for i in range (1,21):
+    if i == 13:
+        pass
+    else:
+        print(i)
+
+----------------------------------------
+
+food = ["pizza", "hamburger", "hotdog", "spaghetti"]
+food[1] = "sushi"
+#print(food)
+#food.append("cheeseburger")
+food.pop(0)
+
+
+#food.remove("pizza")
+for x in food:
+    print(x)
+    -------------------------------------
+       
